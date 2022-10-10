a943731554b687bb80f739b799ec56f16794cdd6
Lucas Burroni <lucas@techforb.com>
2022-10-09 21:50:14 -0300

    solvedCORS?

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
-var bodyParser = require("body-parser");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 
